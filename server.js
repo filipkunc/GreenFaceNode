7ba15371ff0ca844eea2945da04f108236e70fed
Filip Kunc <kunc.filip@gmail.com>
2015-07-21 22:36:38 +0200

    Repo setup

---
+++ server.js
@@ -0,0 +1,39 @@
+var WebSocketServer = require('ws').Server
+    , http = require('http')
+    , express = require('express')
+    , app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+var server = http.createServer(app);
+
+var clientId = 0;
+var players = {};
+
+var wss = new WebSocketServer({server: server});
+
+wss.broadcast = function broadcast(data) {
+    var message = JSON.stringify(data);
+    wss.clients.forEach(function each(client) {
+        client.send(message);
+    });
+};
+
+wss.on('connection', function(ws) {
+    ws.on('message', function(message) {
+        var data = JSON.parse(message);
+        players[data.id] = data.state;
+    });
+    ws.on('close', function() {
+    });
+    clientId++;
+    var initMessage = {type: "init", id: clientId};
+    ws.send(JSON.stringify(initMessage));
+});
+
+server.listen(1337, function() {
+});
+
+setInterval(function() {
+    wss.broadcast({type: "playerStates", playerStates: players});
+}, 100);
